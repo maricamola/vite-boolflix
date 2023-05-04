e0a1bda4153fe8a5219600dd0f1650c7b926c38c
maricamola <maricamola98@libero.it>
2023-05-04 15:05:11 +0200

    logo & favicon added

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,27 @@
 <script>
-export default {
+import Header from './components/Header.vue'
+import Main from './components/Main.vue'
+import Footer from './components/Footer.vue'
 
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Main,
+    Footer
+  }
 }
 </script>
 
-<template></template>
+<template>
+  <Header />
 
-<style></style>
+  <Main />
+
+  <Footer />
+</template>
+
+<style lang="scss">
+@use './scss/main'
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+<template>
+  <header>
+    <div class="logo">
+      <img src="logo-boolflix.png" alt="logo">
+    </div>
+  </header>
+</template>
+
+
+<style lang="scss" scoped>
+header {
+  img {
+    width: 10%;
+    margin: 20px;
+  }
+}
+</style>
